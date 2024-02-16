4900aa519c01bce3dd788a66ded4fe5f419e6ab7
MartimGP <mgalloproenca@gmail.com>
2024-02-16 14:36:37 -0300

    Add devData folder for app

---
+++ app.py
@@ -0,0 +1,25 @@
+import dash
+from dash import dcc
+from dash import html
+
+app = dash.Dash(__name__)
+server = app.server
+
+app.layout = html.Div(children=[
+    html.H1("Estudos Sobre Principios de Chuva em Cidades Brasileiras"),
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montreal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
